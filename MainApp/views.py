32eb93a605b9e40cde01b77cc1c13d5123be2a49
Tushkov <TushkovEV@neftisa.ru>
2024-06-07 14:55:15 +0300

    Формы

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,16 +1,32 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-
+from MainApp.forms import SnippetForm
+ 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'GET':
+        form = SnippetForm()
+        context = {"pagename" : "Добавление нового сниппета",
+                   "form" : form
+                   }
+        return render(request, 'pages/add_snippet.html', context)
 
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+        return render(request, "pages/add_snippet.html", {"form": form})
+        
+def delete_snippet_page(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    snippet.delete()    
+    return redirect('list')
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
@@ -27,12 +43,11 @@ def detail_snippet_page(request, snippet_id):
     }
     return render(request, 'pages/detail.snippet.html', context)
 
-def create_snippet_page(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        lang = request.POST['lang']
-        code = request.POST['code']
-        snippet = Snippet(name=name, lang=lang, code=code)
-        snippet.save()
-    return redirect('list')
-
+# def create_snippet_page(request):
+#     if request.method == 'POST':
+#         name = request.POST['name']
+#         lang = request.POST['lang']
+#         code = request.POST['code']
+#         snippet = Snippet(name=name, lang=lang, code=code)
+#         snippet.save()
+#     return redirect('list')
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name="add"),
     path('snippets/list', views.snippets_page, name="list"),
     path('snippets/<int:snippet_id>', views.detail_snippet_page, name="detail"),
-    path('snippets/create', views.create_snippet_page, name="create"),
+    path('snippets/delete/<int:snippet_id>', views.delete_snippet_page, name="delete"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
